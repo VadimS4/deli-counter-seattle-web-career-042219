e4e9e41e94bbc94f7d185ba00f7e7f715a9acca8
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 09:10:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def take_a_number(array, name)
   array.push(name)
   position = array.index
   puts "Welcome #{name}. You are number #{position + 1} in line"
-  
+  return name, position
   
 
 def line(array)
