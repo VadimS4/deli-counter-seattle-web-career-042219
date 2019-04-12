9c2a1c63e16da69704bb0f77172a8efb971bedfb
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 09:03:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-    array.each_with_index{ |#{counter += 1} #{
+    array.each
       new_line.push("The line is currently: #{counter} #{line}")
       counter += 1
     end
