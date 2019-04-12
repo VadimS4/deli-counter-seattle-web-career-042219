e266e4794a4c65a64ef2133c792f6ba2cb7b4a95
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 09:12:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,12 +9,11 @@ end
   
 
 def line(array)
-  new_line = []
   if array.length == 0
     puts "The line is currently empty."
   else 
       array.each_with_index do |value, index|
-        new_line.push("#{index.to_i+1}. #{value}"
+        push("#{index.to_i+1}. #{value}"
       end
     end
   return new_line
