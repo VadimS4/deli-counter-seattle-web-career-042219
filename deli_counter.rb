cdd9ce1e72af041747b91a61a44120306cb8b069
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 08:47:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 def line(array = katz_deli)
   if array.length < 1
     puts "The line is currently empty."
-  elsif array
+  elsif array.length > 1
+    counter = 0
+    array.each do |place|
+      
+      
     
 end
 
