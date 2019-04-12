19638ef8bf90dc185f7e96d4ff6e6f7638a42fa8
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 08:50:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,14 @@
 def line(array = katz_deli)
   if array.length < 1
     puts "The line is currently empty."
-  elsif array.length > 1
+  else 
     counter = 0
     new_line = []
     array.each do |place|
-      new_line.push("The line is currently: #{counter}")
-      
-    
+      new_line.push("The line is currently: #{counter} #{place}")
+      counter += 1
+    end
+  return new_line
 end
 
 def take_a_number(katz_deli, string)
